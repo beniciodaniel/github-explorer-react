605b27bd71d55af1421fea2cf7b1b63ec328f7a4
Benício Barreto <benicio.daniel@gmail.com>
2020-09-04 15:18:08 -0300

    Added logo and applied styles to input

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import { Title } from './styles';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import logoImg from '../../assets/github-logo.svg';
+
+import { Title, Form } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do respositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+  </>
+);
 
 export default Dashboard;
